caa6af4fc8ca39dd3ced9c277fc331286a33aa06
A01721729 <A01721729@itesm.mx>
2021-08-29 22:01:22 -0500

    enter

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,6 +1,19 @@
 
 def main():
-    print("Jesus Gonzalez")
+    edad = int(input("Ingresa tu edad: "))
+    identificacion = input("¿Tienes identificación oficial? (s/n): ")
+
+    if((identificacion != "s" and identificacion != "n") or (edad<0)):
+        print("Respuesta incorrecta")
+
+    elif(edad>=18 and identificacion =="s"):
+        print("Trámite de licencia concedido")
+
+    else: 
+        print("No cumples requisitos")
+
+
+
 
 
 if __name__ == '__main__':
